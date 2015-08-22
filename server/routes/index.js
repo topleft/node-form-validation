dde94b318d56353bbc13e59c32936b6a5a1eed0b
topleft <pete.james.jeffryes@gmail.com>
2015-08-22 17:41:55 -0600

    finished form template, outlined routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
+// puppies home page with choices, see puppies, create puppies
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//  get route for create puppies
+router.get("/puppies/new", function(req, res){
+  res.render("form");
+});
+
+// post route recieving puppy info
+router.post("/submit", function(req, res){
+  res.render("puppies", {})
+});
+
+//  get route show puppies info in a table
+router.get("/puppies", function(req, res){
+  res.render("puppies", {});
+})
+
 
 module.exports = router;
