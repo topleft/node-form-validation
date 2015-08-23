9398b3c335dfe6ee35c06df059e251ceea632009
topleft <pete.james.jeffryes@gmail.com>
2015-08-22 20:45:30 -0600

    successfully checking for duplicates, sending alert or wsuccess msg

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,15 +16,49 @@ router.get("/new", function(req, res){
 router.post("/submit", function(req, res){
   var name = req.body.name;
   var id = req.body.id;
-  puppies.push({name: name, id: id})
   console.log(puppies);
-  res.render("puppies", {puppies: puppies});
+  var alert = checkForDuplicate(id, name, puppies, res);
+  if (alert){
+    res.render("puppies", {puppies: puppies, alert: alert, color: "red"});
+  }
+  else{
+    puppies.push({name: name, id: id})
+    console.log(puppies);
+    res.render("puppies", {puppies: puppies, success: name+" added to the system"});
+  }
 });
 
-// get route show puppies info in a table
-// router.get("/puppies", function(req, res){
-//   res.render("puppies");
-// })
+// function checkForDuplicate(currentId, currentName, arr){
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i]["id"] === currentId && arr[i]["name"] === currentName){
+//       var alert = "That pup is already in the system.";;
+//       return alert;
+//     }
+//     else if (arr[i]["id"] === currentId){
+//       var alert = currentId+" is taken, please choose another.";
+//       return alert;
+//     }
+//   };
+// };
+
+// need to figure out how to render form again.
+
+function checkForDuplicate(currentId, currentName, arr, res){
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i]["id"] === currentId && arr[i]["name"] === currentName){
+      var alert = "That pup is already in the system.";
+      // res.render("puppies", {alert: alert, puppies: puppies});
+      return alert;
+    }
+    else if (arr[i]["id"] === currentId){
+      var alert = currentId+" is taken, please choose another.";
+      // res.render("puppies", {alert: alert, puppies: puppies});
+      return alert;
+    }
+    else
+      return false;
+  };
+};
 
 
 module.exports = router;
