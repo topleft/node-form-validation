d462d61fa8b34c908660ba559efbea9d1d617b70
topleft <pete.james.jeffryes@gmail.com>
2015-08-22 18:18:54 -0600

    form info is rendering on table

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,19 +8,22 @@ router.get('/', function(req, res, next) {
 });
 
 //  get route for create puppies
-router.get("/puppies/new", function(req, res){
+router.get("/new", function(req, res){
   res.render("form");
 });
 
 // post route recieving puppy info
 router.post("/submit", function(req, res){
-  res.render("puppies", {})
+  var name = req.body.name;
+  var id = req.body.id;
+  console.log(name);
+  res.render("puppies", {name: name, id: id});
 });
 
-//  get route show puppies info in a table
-router.get("/puppies", function(req, res){
-  res.render("puppies", {});
-})
+// get route show puppies info in a table
+// router.get("/puppies", function(req, res){
+//   res.render("puppies");
+// })
 
 
 module.exports = router;
